fdbc77e60b741c5085fb47435700d55d41611dc0
tkoch97 <tkoch.dev@gmail.com>
2025-10-23 12:04:17 -0300

    feat: update Register method on User controller to execute RegisterUserUseCase.

--- src/Backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
 
@@ -11,9 +11,11 @@ namespace MyRecipeBook.API.Controllers
     {
         [HttpPost("register")]
         [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
-        public IActionResult Register(RequestRegisterUserJson request)
+        public async Task<IActionResult> Register([FromServices] IRegisterUserUseCase useCase,[FromBody] RequestRegisterUserJson request)
         {
-            return Created();
+            var useCaseResponse = await useCase.Execute(request);
+
+            return Created(string.Empty, useCaseResponse);
         }
     }
 }
