91eac8e720d9fa63202ed2a97b793b8ad7c38331
tkoch97 <tkoch.dev@gmail.com>
2025-10-23 11:43:17 -0300

    feat: add dependency injection archive.

---
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Application.Services.AutoMapper;
+using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Application.UseCases.User.Register;
+
+namespace MyRecipeBook.Application
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddAutoMapper(services);
+            AddUseCases(services);
+            AddPasswordEncrypter(services, configuration);
+        }
+
+        private static void AddUseCases(IServiceCollection services)
+        {
+            services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        }
+
+        private static void AddAutoMapper(IServiceCollection services)
+        {
+            services.AddScoped(option => new AutoMapper.MapperConfiguration(options =>
+            {
+                options.AddProfile(new AutoMapping());
+            }).CreateMapper());
+        }
+
+        private static void AddPasswordEncrypter(IServiceCollection services, IConfiguration configuration)
+        {
+            var additionalKey = configuration.GetValue<string>("Settings:Passwords:AdditionalKey");
+            services.AddScoped(options => new PasswordEncrypter(additionalKey!));
+        }
+    }
+}
