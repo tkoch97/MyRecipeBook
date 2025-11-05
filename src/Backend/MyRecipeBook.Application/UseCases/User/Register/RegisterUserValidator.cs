121b6f5c3f908c6f8e4de0dd96c642580ffc25bc
tkoch97 <tkoch.dev@gmail.com>
2025-11-05 10:28:11 -0300

    refactor: separate empty email address and invalid email address rule in RegisterUserValidator. Change the JSON response from the execute function in RegisterUserUseCase to now retrieve the name of registered user.

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -4,17 +4,17 @@ using MyRecipeBook.Exceptions;
 
 namespace MyRecipeBook.Application.UseCases.User.Register
 {
-    internal class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
     {
         public RegisterUserValidator() 
         {
             RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessageException.NAME_EMPTY);
-            RuleFor(user => user.Email)
-                .NotEmpty().WithMessage(ResourceMessageException.EMAIL_EMPTY)
-                .EmailAddress().WithMessage(ResourceMessageException.EMAIL_VALID);
-            RuleFor(user => user.Password.Length)
-                .GreaterThanOrEqualTo(6).WithMessage(ResourceMessageException.PASSWORD_LENGTH);
-    
+            RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessageException.EMAIL_EMPTY);
+            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessageException.PASSWORD_LENGTH);
+            When(user => string.IsNullOrEmpty(user.Email) == false, () =>
+            {
+                RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessageException.EMAIL_VALID);
+            });
         }
     }
 }
