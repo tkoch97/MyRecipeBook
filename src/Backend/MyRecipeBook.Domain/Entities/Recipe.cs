0fe97235ec26b89861d1ea2195015890f2aedddc
tkoch97 <tkoch.dev@gmail.com>
2026-01-28 17:19:27 -0300

    feat: add Recipe entitie

---
+++ src/Backend/MyRecipeBook.Domain/Entities/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using MyRecipeBook.Domain.Enums;
+
+namespace MyRecipeBook.Domain.Entities
+{
+    public class Recipe : EntityBase
+    {
+        public string Title { get; set; } = string.Empty;
+        public CookingTime? CookingTime { get; set; }
+        public Difficulty? Difficulty { get; set; }
+        public IList<Ingredient> Ingredients { get; set; } = []; // Inicializa como uma lista vazia, evitando null reference exceptions
+        public IList<Instruction> Instructions { get; set; } = [];
+        public List<DishType> DishTypes { get; set; } = [];
+        public long UserId { get; set; }
+    }
+}
