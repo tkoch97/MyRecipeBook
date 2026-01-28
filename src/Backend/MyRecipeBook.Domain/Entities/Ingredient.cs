ee65c155b486f8ec5c79fb392d8f80ad017b9b8f
tkoch97 <tkoch.dev@gmail.com>
2026-01-28 17:20:14 -0300

    feat: add Ingredient entitie

---
+++ src/Backend/MyRecipeBook.Domain/Entities/Ingredient.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipeBook.Domain.Entities
+{
+    public class Ingredient : EntityBase
+    {
+        public string Item { get; set; } = string.Empty;
+        public long RecipeId { get; set; }
+    }
+}
