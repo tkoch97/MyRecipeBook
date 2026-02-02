401ef8a7fd7dfefc0f2e637e20cf8f2a24a44f48
tkoch97 <tkoch.dev@gmail.com>
2026-02-02 18:54:07 -0300

    feat: add functions to identify and choice beetwen database type

--- src/Backend/MyRecipeBook.Infrastructure/Extensions/ConfigurationExtension.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using MyRecipeBook.Domain.Enums;
 
 namespace MyRecipeBook.Infrastructure.Extensions
 {
@@ -9,9 +10,19 @@ namespace MyRecipeBook.Infrastructure.Extensions
             return configuration.GetValue<bool>("InMemoryTest");
         }
 
+        public static DatabaseType DatabaseType(this IConfiguration configuration)
+        {
+            var databaseType = configuration.GetConnectionString("DatabaseType");
+            return (DatabaseType)Enum.Parse(typeof(DatabaseType), databaseType!);
+        }
+
         public static string ConnectionString(this IConfiguration configuration)
         {
-            return configuration.GetConnectionString("ConnectionMySQLServer")!;
+            var dataBaseType = configuration.DatabaseType();
+            if(dataBaseType == Domain.Enums.DatabaseType.MySql)
+                return configuration.GetConnectionString("ConnectionMySQLServer")!;
+            else
+                return configuration.GetConnectionString("ConnectionSqlServer")!;
         }
     }
 }
