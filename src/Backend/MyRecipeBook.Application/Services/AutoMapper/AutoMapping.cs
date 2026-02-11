de89eea92c572855d25137d570bdda30f4929f88
tkoch97 <tkoch.dev@gmail.com>
2026-02-11 18:18:04 -0300

    feat: add mapper to RequestRecipeJson

--- src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
+++ src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using MyRecipeBook.Communication.Enums;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
 
@@ -16,11 +17,26 @@ namespace MyRecipeBook.Application.Services.AutoMapper
         {
             CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
                .ForMember(destination => destination.Password, option => option.Ignore()); //Seleciona a propriedade Password para ser ignorada no mapeamento
+
+            CreateMap<RequestRecipeJson, Domain.Entities.Recipe>()
+                .ForMember(destination => destination.Instructions, option => option.Ignore())
+                .ForMember(destination => destination.Ingredients, option => option.MapFrom(source => source.Ingredients.Distinct())) //Para impedir que o usuário consiga registars ingredientes duplicados
+                .ForMember(destination => destination.DishTypes, option => option.MapFrom(source => source.DishTypes.Distinct()));
+
+            CreateMap<string, Domain.Entities.Ingredient>()
+                .ForMember(destination => destination.Item, option => option.MapFrom(source => source));
+
+            CreateMap<DishType, Domain.Entities.DishType>()
+                .ForMember(destination => destination.Type, option => option.MapFrom(source => source));
+
+            CreateMap<RequestInstructionJson, Domain.Entities.Instruction>();
         }
 
         private void DomainToResponse()
         {
             CreateMap<Domain.Entities.User, ResponseUserProfileJson>();
+            CreateMap<Domain.Entities.Recipe, ResponseRegisteredRecipeJson>();
+
         }
     }
 }
