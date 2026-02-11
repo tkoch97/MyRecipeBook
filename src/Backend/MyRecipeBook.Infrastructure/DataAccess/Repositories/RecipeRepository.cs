56c059f41a80e98bc3ff611654d62b002fdf2f5a
tkoch97 <tkoch.dev@gmail.com>
2026-02-11 15:50:03 -0300

    feat: add RecipeRepository

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/RecipeRepository.cs
@@ -0,0 +1,17 @@
+﻿using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Repositories.Recipe;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Repositories
+{
+    public class RecipeRepository : IRecipeWriteOnlyRepository
+    {
+        private readonly MyRecipeBookDbContext _dbContext;
+
+        public RecipeRepository(MyRecipeBookDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add(Recipe recipe) => await _dbContext.Recipes.AddAsync(recipe);
+    }
+}
