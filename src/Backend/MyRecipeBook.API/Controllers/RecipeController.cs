e118df6c66c4c39e2a60c6e55f21d03339c660aa
tkoch97 <tkoch.dev@gmail.com>
2026-02-11 18:17:26 -0300

    feat: add controller to register recipe

---
+++ src/Backend/MyRecipeBook.API/Controllers/RecipeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.API.Attributes;
+using MyRecipeBook.Application.UseCases.Recipe.Register;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers
+{
+    [AuthenticatedUser]
+    public class RecipeController : MyRecipeBookController
+    {
+        [HttpPost("register")]
+        [ProducesResponseType(typeof(ResponseRegisteredRecipeJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register
+            (
+                [FromServices] IRegisterRecipeUseCase useCase, 
+                [FromBody] RequestRecipeJson request
+            )
+        {
+            var useCaseResponse = await useCase.Execute(request);
+
+            return Created(string.Empty, useCaseResponse);
+        }
+    }
+}
