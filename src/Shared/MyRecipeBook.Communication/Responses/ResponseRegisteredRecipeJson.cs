139b9c7c526ee24d7b2cbb5c6d25d54c03e7709f
tkoch97 <tkoch.dev@gmail.com>
2026-02-20 15:46:20 -0300

    feat: add attributes in ResponseRegisteredRecipeJson

--- src/Shared/MyRecipeBook.Communication/Responses/ResponseRegisteredRecipeJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseRegisteredRecipeJson.cs
@@ -2,6 +2,8 @@
 {
     public class ResponseRegisteredRecipeJson
     {
+        public string Id { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
 
     }
 }
