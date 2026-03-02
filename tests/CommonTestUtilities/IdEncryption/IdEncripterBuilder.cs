6008277767a626200c336480d580863cb27aea8a
tkoch97 <tkoch.dev@gmail.com>
2026-03-02 16:03:47 -0300

    test: add RegisterRecipeUseCaseTest

---
+++ tests/CommonTestUtilities/IdEncryption/IdEncripterBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Sqids;
+
+namespace CommonTestUtilities.IdEncryption
+{
+    public class IdEncripterBuilder
+    {
+        public static SqidsEncoder<long> Build()
+        {
+            return new SqidsEncoder<long>(new()
+            {
+                MinLength = 3,
+                Alphabet = "l6AS3KUGXtnOz4jhf28rvpEI5b1Q97CMd"
+            });
+        }
+    }
+}
