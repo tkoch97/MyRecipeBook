efa3947edf0182b70d5b0657181e918d93899afa
tkoch97 <tkoch.dev@gmail.com>
2026-03-17 14:15:56 -0300

    feat: add controller

--- src/Backend/MyRecipeBook.API/Controllers/RecipeController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/RecipeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyRecipeBook.API.Attributes;
+using MyRecipeBook.Application.UseCases.Recipe.Filter;
 using MyRecipeBook.Application.UseCases.Recipe.Register;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
@@ -21,6 +22,26 @@ namespace MyRecipeBook.API.Controllers
             var useCaseResponse = await useCase.Execute(request);
 
             return Created(string.Empty, useCaseResponse);
+        }
+
+        [HttpPost("filter")]
+        [ProducesResponseType(typeof(ResponseRecipesJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> Filter
+            (
+                [FromServices] IFilterRecipeUseCase useCase,
+                [FromBody] RequestFilterRecipeJson request
+            )
+        {
+            var useCaseResponse = await useCase.Execute(request);
+
+            if (useCaseResponse.Recipes.Any())
+            {
+                return Ok(useCaseResponse);
+            }
+
+            return NoContent();
         }
     }
 }
