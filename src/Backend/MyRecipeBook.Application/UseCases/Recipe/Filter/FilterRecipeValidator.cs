ede1b078a1c76ee383f2eab3ff5022e067fff71b
tkoch97 <tkoch.dev@gmail.com>
2026-03-24 11:42:39 -0300

    feat: add validator to filter recipes

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/Filter/FilterRecipeValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.Filter
+{
+    public class FilterRecipeValidator : AbstractValidator<RequestFilterRecipeJson>
+    {
+        public FilterRecipeValidator()
+        {
+            RuleForEach(r => r.CookingTimes).IsInEnum().WithMessage(ResourceMessageException.COOKING_TIME_NOT_SUPPORTED);
+            RuleForEach(r => r.Difficulties).IsInEnum().WithMessage(ResourceMessageException.DIFFICULTY_LEVEL_NOT_SUPPORTED);
+            RuleForEach(r => r.DishTypes).IsInEnum().WithMessage(ResourceMessageException.DISH_TYPE_NOT_SUPPORTED);
+        }
+    }
+}
