9cdbd16d31ee7b45059c32e58c37142b5e8ffb07
tkoch97 <tkoch.dev@gmail.com>
2026-03-24 11:37:09 -0300

    feat: add dto to filter recipes controller

---
+++ src/Backend/MyRecipeBook.Domain/Dtos/FilterRecipesDto.cs
@@ -0,0 +1,12 @@
+﻿using MyRecipeBook.Domain.Enums;
+
+namespace MyRecipeBook.Domain.Dtos
+{
+    public record FilterRecipesDto
+    {
+        public string? RecipeTitle_Ingredients { get; init; }
+        public IList<CookingTime> CookingTimes { get; init; } = [];
+        public IList<Difficulty> Difficulties { get; init; } = [];
+        public IList<DishType> DishTypes { get; init; } = [];
+    }
+}
