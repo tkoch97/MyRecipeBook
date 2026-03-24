fe6955924399b7432f024e7908c102445d302552
tkoch97 <tkoch.dev@gmail.com>
2026-03-24 11:44:01 -0300

    add: add usecase to filter recipes

--- src/Backend/MyRecipeBook.Application/UseCases/Recipe/Filter/FilterRecipeUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/Filter/FilterRecipeUseCase.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Repositories.Recipe;
 using MyRecipeBook.Domain.Services.LoggedUser;
+using MyRecipeBook.Exceptions.ExceptionsBase;
 
 namespace MyRecipeBook.Application.UseCases.Recipe.Filter
 {
@@ -9,16 +11,49 @@ namespace MyRecipeBook.Application.UseCases.Recipe.Filter
     {
         private readonly IMapper _mapper;
         private readonly ILoggedUser _loggedUser;
+        private readonly IRecipeReadOnlyRepository _recipeReadOnlyRepository;
 
-        public FilterRecipeUseCase(IMapper mapper, ILoggedUser loggedUser)
+        public FilterRecipeUseCase(IMapper mapper, ILoggedUser loggedUser, IRecipeReadOnlyRepository recipeReadOnlyRepository)
         {
             _mapper = mapper;
             _loggedUser = loggedUser;
+            _recipeReadOnlyRepository = recipeReadOnlyRepository;
         }
 
-        public Task<ResponseRecipesJson> Execute(RequestFilterRecipeJson request)
+        public async Task<ResponseRecipesJson> Execute(RequestFilterRecipeJson request)
         {
-            throw new NotImplementedException();
+            Validate(request);
+
+            var loggedUser = await _loggedUser.User();
+
+            var filters = new Domain.Dtos.FilterRecipesDto
+            {
+                RecipeTitle_Ingredients = request.RecipeTitle_Ingredient,
+                CookingTimes = request.CookingTimes.Distinct().Select(c => (Domain.Enums.CookingTime)c).ToList(),
+                Difficulties = request.Difficulties.Distinct().Select(c => (Domain.Enums.Difficulty)c).ToList(),
+                DishTypes = request.DishTypes.Distinct().Select(c => (Domain.Enums.DishType)c).ToList(),
+            };
+
+            var recipes = await _recipeReadOnlyRepository.Filter(loggedUser, filters);
+
+            return new ResponseRecipesJson
+            {
+                Recipes = _mapper.Map<List<ResponseShortRecipeJson>>(recipes)
+            };
+        }
+
+        private static void Validate(RequestFilterRecipeJson request)
+        {
+            var validator = new FilterRecipeValidator();
+
+            var result = validator.Validate(request);
+
+            if (!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(e => e.ErrorMessage).Distinct().ToList();
+
+                throw new ErrorOnValidationException(errorMessages);
+            }
         }
     }
 }
